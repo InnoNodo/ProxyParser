49755eb9aaf1c8624c56baa289bf7048ee47cbf0
InnoNodo <k.nosov@innopolis.university>
2025-02-23 17:48:00 +0300

    Added option to add the prefix

--- parser.py
+++ parser.py
@@ -2,6 +2,17 @@ import requests
 
 file = open('proxies.txt', 'w')
 
+while True:
+    flag = input(f"Do you want to get prefix before the proxy? (y/n) ")
+    if flag == 'y' or flag == 'yes':
+        flag = 1
+        break
+    elif flag == 'n' or flag == 'no':
+        flag=0
+        break
+    else:
+        print("Choose correct option.")
+
 print("Choose the type (socks4 | socks5 | http)")
 
 while True:
@@ -9,21 +20,24 @@ while True:
     if choice == 'socks4':
         response = requests.get('https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt')
         for line in response.text.splitlines():
-            newLine = 'socks4://' + line + '\n'
+            if flag: newLine = 'socks4://' + line + '\n'
+            else: newLine = line + '\n'
             file.write(newLine)
         print("Your file is ready (proxies.txt)")
         break
     elif choice == 'socks5':
         response = requests.get('https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt')
         for line in response.text.splitlines():
-            newLine = 'socks5://' + line + '\n'
+            if flag: newLine = 'socks5://' + line + '\n'
+            else: newLine = line + '\n'
             file.write(newLine)
         print("Your file is ready (proxies.txt)")
         break
     elif choice == 'http':
         response = requests.get('https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt')
         for line in response.text.splitlines():
-            newLine = 'http://' + line + '\n'
+            if flag: newLine = 'http://' + line + '\n'
+            else: newLine = line + '\n'
             file.write(newLine)
         print("Your file is ready (proxies.txt)")
         break
