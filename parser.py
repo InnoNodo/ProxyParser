54ac950420191919263953f10c4530b2839d7bc5
InnoNodo <k.nosov@innopolis.university>
2025-02-23 17:31:24 +0300

    Fixed prefix for proxies

--- parser.py
+++ parser.py
@@ -16,14 +16,14 @@ while True:
     elif choice == 'socks5':
         response = requests.get('https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt')
         for line in response.text.splitlines():
-            newLine = 'socks4://' + line + '\n'
+            newLine = 'socks5://' + line + '\n'
             file.write(newLine)
         print("Your file is ready (proxies.txt)")
         break
     elif choice == 'http':
         response = requests.get('https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt')
         for line in response.text.splitlines():
-            newLine = 'socks4://' + line + '\n'
+            newLine = 'http://' + line + '\n'
             file.write(newLine)
         print("Your file is ready (proxies.txt)")
         break
