e8ae4ecf779a4c219eef5c18930ba6636dd960f6
InnoNodo <k.nosov@innopolis.university>
2025-02-23 17:24:28 +0300

    Added the python parser for socks4, socks5, http

---
+++ parser.py
@@ -0,0 +1,32 @@
+import requests
+
+file = open('proxies.txt', 'w')
+
+print("Choose the type (socks4 | socks5 | http)")
+
+while True:
+    choice = input(f"Type your choice: ")
+    if choice == 'socks4':
+        response = requests.get('https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt')
+        for line in response.text.splitlines():
+            newLine = 'socks4://' + line + '\n'
+            file.write(newLine)
+        print("Your file is ready (proxies.txt)")
+        break
+    elif choice == 'socks5':
+        response = requests.get('https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt')
+        for line in response.text.splitlines():
+            newLine = 'socks4://' + line + '\n'
+            file.write(newLine)
+        print("Your file is ready (proxies.txt)")
+        break
+    elif choice == 'http':
+        response = requests.get('https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt')
+        for line in response.text.splitlines():
+            newLine = 'socks4://' + line + '\n'
+            file.write(newLine)
+        print("Your file is ready (proxies.txt)")
+        break
+    else:
+        print("Try again")
+
